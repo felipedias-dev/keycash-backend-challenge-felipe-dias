9f552ade967aa35a19bf1e9c0fd28124933ab562
Felipe Dias <felipedias.dev@gmail.com>
2021-10-22 01:11:40 -0300

    refactor routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,18 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import {
+  createCategoryController,
+  listCategoriesController,
+} from '../useCases';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const result = categoriesRepository.list();
-
-  return response.json(result);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
