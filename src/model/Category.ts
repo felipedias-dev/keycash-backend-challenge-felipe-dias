d1a955c4bf51b27c65e9303dcd5e1710e9881d47
Felipe Dias <felipedias.dev@gmail.com>
2021-10-21 22:01:55 -0300

    add Category model

---
+++ src/model/Category.ts
@@ -0,0 +1,18 @@
+import { v4 as uuidV4 } from 'uuid';
+
+class Category {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+  updated_at?: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+    this.updated_at = new Date();
+  }
+}
+
+export { Category };
